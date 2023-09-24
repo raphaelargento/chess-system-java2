416df89df9c728a260bde8e16072e0c55374af92
Raphael Argento <raphaelargento@gmail.com>
2023-09-24 01:39:13 -0300

    Starting go implement Board and Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
 import boardgame.Position;
+import boardgame.Board;
 
 public class Program {
     public static void main(String[] args) throws Exception {
-        Position pos = new Position(3, 5);
-        System.out.println(pos);
+            // Verificação da posição
+            Position pos = new Position(3, 5);
+            System.out.println(pos);
+
+            // Impletentação do tabuleiro:
+            Board board = new Board(8, 8);
     }
 }
